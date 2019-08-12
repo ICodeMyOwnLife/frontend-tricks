47384a1746f25a466d3478b59b85a174a3ea55b2
Minh Hiền Trương <hien.truong@grabtaxi.com>
2019-08-12 18:28:05 +0700

    Add

--- src/questions/css/TextEllipsis1/index.tsx
+++ src/questions/css/TextEllipsis1/index.tsx
@@ -1,28 +1,49 @@
 import React, { FC, memo } from 'react';
+import classnames from 'classnames';
 import Question, { QuestionInfo } from 'components/Question';
 import classes from './styles.module.scss';
 
 const questions: QuestionInfo[] = [
   {
-    question: `How to truncate long text in table column and replace with ellipsis?`,
+    question: `Truncate long text in table column and replace with ellipsis?`,
     answer: (
-      <pre>
-        <code>
-          {`td {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  
-  &.Td1 {
-    max-width: 0; // this column will take the remaining width
-  }
-  
-  &.Td4 {
-    max-width: 100px; // this column will take 100px
-  }
-}`}
-        </code>
-      </pre>
+      <div>
+        <p>Case 1: Other columns are fixed-width</p>
+        <pre>
+          <code>
+            {`td {
+      white-space: nowrap;
+      
+    &.Td1 {
+      max-width: 0; // this column will take the remaining width
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    
+    &.Td4 {
+      max-width: 100px; // this column will take 100px
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }`}
+          </code>
+        </pre>
+        <p>Case 2: Other columns are flexible-width</p>
+        <pre>
+          <code>
+            {`td {
+      white-space: nowrap;
+      
+    &.Td1 {
+      width: 100%;
+      max-width: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }`}
+          </code>
+        </pre>
+      </div>
     ),
     references: [
       {
@@ -43,14 +64,13 @@ const questions: QuestionInfo[] = [
 
 export const TextEllipsis1Impl: FC = () => (
   <Question title="Text Ellipsis 1" questions={questions}>
-    <table className={classes.Table}>
+    <table className={classnames(classes.Table, classes.Table1)}>
       <colgroup>
         <col className={classes.Col1} />
         <col className={classes.Col2} />
         <col className={classes.Col3} />
         <col className={classes.Col4} />
       </colgroup>
-
       <thead>
         <tr>
           <th>Col 1</th>
@@ -70,6 +90,37 @@ export const TextEllipsis1Impl: FC = () => (
             <td>This is a small text.</td>
             <td>This is a small text.</td>
             <td className={classes.Td4}>This is another quite long text.</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
+    <table className={classnames(classes.Table, classes.Table2)}>
+      <thead>
+        <tr>
+          <th>Column 1</th>
+          <th>Col 2</th>
+          <th>Column Header 3</th>
+          <th>4</th>
+          <th>C5</th>
+          <th>#6</th>
+          <th>No. 7</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {[1, 2, 3, 4, 5].map(n => (
+          <tr key={n}>
+            <td className={classes.Td1}>
+              This a very long text and a part of it should be truncated and
+              replaced with ellipsis or else the layout is broken.
+            </td>
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
+            <td />
           </tr>
         ))}
       </tbody>
