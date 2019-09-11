a16f0070ce459d598b1a78d1868109cb5392b489
ICodeMyOwnLife <icodemyownlife@gmail.com>
2019-09-11 08:07:33 +0700

    Add

--- src/questions/dom/CancelRequest1/index.tsx
+++ src/questions/dom/CancelRequest1/index.tsx
@@ -8,6 +8,14 @@ export const CancelRequest1Impl: FC = () => (
   </Question>
 );
 
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+// https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal
+// https://developer.mozilla.org/en-US/docs/Web/API/AbortController
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/abort
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+// https://github.com/axios/axios
+
 const CancelRequest1 = memo(CancelRequest1Impl);
 CancelRequest1.displayName = 'CancelRequest1';
 export default CancelRequest1;
