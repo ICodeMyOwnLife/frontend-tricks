e192d19a3a19bb986eb24eff8dea52c3b13b65fa
ICodeMyOwnLife <icodemyownlife@gmail.com>
2019-11-06 07:12:27 +0700

    Add

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -34,7 +34,15 @@ export default useMediaQuery;
 
 /**
  * # References
+ * https://github.com/ReactTraining/react-media
+ * https://material-ui.com/components/use-media-query/
  * https://github.com/streamich/react-use/blob/master/src/useMedia.ts
  * https://github.com/jaredpalmer/the-platform/blob/master/src/useMedia.tsx
  * https://usehooks.com/useMedia/
+ * https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia
+ * https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList
+ * https://developer.mozilla.org/en-US/docs/Web/CSS/Media_Queries/Testing_media_queries
+ * https://developer.mozilla.org/en-US/docs/Web/CSS/Media_Queries/Using_media_queries
+ * https://gomakethings.com/a-better-way-to-test-breakpoints-with-vanilla-javascript/
+ * https://github.com/akiran/json2mq
  */
