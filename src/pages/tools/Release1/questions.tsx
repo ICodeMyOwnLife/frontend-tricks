4f12749019c77783ffd4afee50a90157d0bd708a
HienTM <hientm@sendo.vn>
2020-03-05 18:29:22 +0700

    docs(tools): add docs for release-it

--- src/pages/tools/Release1/questions.tsx
+++ src/pages/tools/Release1/questions.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import CodeViewer from 'components/CodeViewer';
 import { QuestionInfo } from 'types/app-common';
@@ -20,15 +21,45 @@ const questions: QuestionInfo[] = [
         <CodeViewer language="json">
           {`{
   "git": {
-    "commitMessage": "chore(release): release $\{version}",
+    "commitMessage": "chore(release): release $\{version}"
+  }
+}`}
+        </CodeViewer>
+
+        <p>
+          To support GitHub release: assign GITHUB_TOKEN environment variable
+          and add configs to <code>.release-it.json</code>
+        </p>
+
+        <CodeViewer language="bash">export GITHUB_TOKEN="abc..."</CodeViewer>
+
+        <CodeViewer language="json">
+          {`{
+  "git": {
     "pushRepo": "origin"
   },
   "github": {
     "release": true,
     "origin": "https://github.com"
+  }
+}`}
+        </CodeViewer>
+
+        <p>
+          To support GitLab release: assign GITLAB_TOKEN environment variable
+          and add configs to <code>.release-it.json</code>
+        </p>
+
+        <CodeViewer language="bash">export GITLAB_TOKEN="abc..."</CodeViewer>
+
+        <CodeViewer language="json">
+          {`{
+  "git": {
+    "pushRepo": "gitlab"
   },
-  "npm": {
-    "publish": true
+  "gitlab": {
+    "release": true,
+    "origin": "https://gitlab.com"
   }
 }`}
         </CodeViewer>
@@ -40,6 +71,18 @@ const questions: QuestionInfo[] = [
         <CodeViewer language="json">
           {`"scripts": {
   "release": "release-it --ci"
+}`}
+        </CodeViewer>
+
+        <p>
+          To support NPM publish: add configs to <code>.release-it.json</code>
+        </p>
+
+        <CodeViewer language="json">
+          {`{
+  "npm": {
+    "publish": true
+  }
 }`}
         </CodeViewer>
 
